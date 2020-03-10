9707711c46bdd90e6bf1bb7e7421d1728e646b11
Nicolas <nicolas.fouchard94370@gmail.com>
2020-03-10 12:17:54 +0100

    ADD Robot.h and Robot.cpp

---
+++ SOIL/include/Robot.h
@@ -0,0 +1,37 @@
+#include <GL/gl.h>
+#include <GL/glu.h>
+
+#include "glut.h"
+#include "SOIL.h"
+
+class Robot
+{
+    float *normal;
+    float radius = 0.25;
+    float dabRightArm = 0;
+    float dabLeftArm = 0;
+    float normalArms[2] = {90, 30};
+    short myBool = 0;
+    float rotateRightLeg = 0;
+    float rotateLeftLeg = 0;
+    int rotateRightLegLimit = 0;
+    float rotateRightKnee = 0;
+    float rotateLeftKnee = 0;
+
+    void createCube();
+    void createJetpack();
+    void createChest();
+    void createHexagone();
+    void createRightArm();
+    void createLeftArm();
+    void dab();
+    void getNormalArms();
+    float *vectorProduct(float point1, float point2, float point3, float point4, float point5, float point6, float point7, float point8, float point9);
+    void mouseMove(int x, int y);
+    void setlight();
+    void setMaterial(int mode);
+    void createHead();
+    void walk();
+    void createPelvis();
+    void createLegs();
+};
